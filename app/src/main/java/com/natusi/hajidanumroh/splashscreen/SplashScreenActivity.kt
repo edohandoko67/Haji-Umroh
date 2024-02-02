49c70d0ca76dd98e26c815fa425ef42a5a3df3d8
edohandoko67 <edohandoko67@gmail.com>
2024-02-02 14:35:35 +0700

    HomePage + list

--- app/src/main/java/com/natusi/hajidanumroh/splashscreen/SplashScreenActivity.kt
+++ app/src/main/java/com/natusi/hajidanumroh/splashscreen/SplashScreenActivity.kt
@@ -4,8 +4,8 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
-import com.natusi.hajidanumroh.MainActivity
 import com.natusi.hajidanumroh.databinding.ActivitySplashScreenBinding
+import com.natusi.hajidanumroh.ui.HomeActivity
 
 class SplashScreenActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySplashScreenBinding
@@ -19,7 +19,7 @@ class SplashScreenActivity : AppCompatActivity() {
         val lottieAnimationView = binding.lottieAnimationView
         lottieAnimationView.animate().setDuration(3000)
         Handler().postDelayed({
-            val intent = Intent(this, MainActivity::class.java)
+            val intent = Intent(this, HomeActivity::class.java)
             startActivity(intent)
             finish()
         }, 3000)
