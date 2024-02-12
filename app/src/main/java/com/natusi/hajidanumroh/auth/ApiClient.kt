9bd534ac5099b93dd76f6101f495ddbce5112076
edohandoko67 <edohandoko67@gmail.com>
2024-02-12 10:52:43 +0700

    data paket show

--- app/src/main/java/com/natusi/hajidanumroh/auth/ApiClient.kt
+++ app/src/main/java/com/natusi/hajidanumroh/auth/ApiClient.kt
@@ -7,7 +7,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 object ApiClient {
     private var retrofit: Retrofit? = null
     private const val BASE_URL = "http://192.168.2.111/"
-
+    final const val URL_IMAGE = "http://192.168.2.111/develop/KBIHU_Al-Rahmah/public/foto-paket/"
     fun getRetrofitInstance(): Retrofit? {
         if (retrofit == null) {
             retrofit = Retrofit.Builder()
